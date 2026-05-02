34a846508071b840b7a9b50cdc89e2096aab79d8
escalarencey <escala.rence03@gmail.com>
2026-05-02 13:06:24 +0800

    Update Product.java

--- src/main/java/com/ws101/escala/model/Product.java
+++ src/main/java/com/ws101/escala/model/Product.java
@@ -1,22 +1,25 @@
 package com.ws101.escala.model;
 
+import jakarta.persistence.*;
 import lombok.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
-/**
- * Data entity representing a pet product for the EcommerceApi.
- */
-@Getter 
-@Setter 
-@NoArgsConstructor 
-@AllArgsConstructor 
-@ToString 
-@EqualsAndHashCode
+@Entity
+@Table(name = "products")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
     private String description;
     private Double price;
-    private String category;
     private Integer stockQuantity;
     private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @JsonBackReference
+    private Category category;
 }
