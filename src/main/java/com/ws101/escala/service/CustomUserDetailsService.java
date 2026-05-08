fafcc48a9452f6fe0dfecd2588afab51884cab61
escalarencey <escala.rence03@gmail.com>
2026-05-08 16:53:30 +0800

    Create CustomUserDetailsService.java

---
+++ src/main/java/com/ws101/escala/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.ws101.escala.service;
+
+import com.ws101.escala.model.User;
+import com.ws101.escala.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService { // Task 1.2: Service
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
